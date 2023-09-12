d7975cf0cc735509b199a55fb7a30b6e2614960b
Rayyan (Rayy) <rayyan.hodges@studytafensw.edu.au>
2023-09-12 11:13:24 +0000

    Adding user to Firebase

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -14,6 +14,7 @@ export function Signup ( props ) {
     const[validpassword,setValidpassword] = useState(false)
     const submitHandler = (evt) => {
         evt.preventDefault()
+        props.handler(useremail, password)
     }
 
     useEffect( () => {
