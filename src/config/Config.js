a50e00fb45215dc1491364d8801bc31eaeee5742
Rayyan (Rayy) <rayyan.hodges@studytafensw.edu.au>
2023-11-07 23:52:27 +1100

    Change FirebaseConfig to correct authkey

--- src/config/Config.js
+++ src/config/Config.js
@@ -1,9 +1,8 @@
-export const FirebaseConfig = {
-    apiKey: "AIzaSyAL9_SCfMisIz7-kwV9ZBZzlG7y4lsGKdc",
-    authDomain: "rayyan-h-tafe-nsw.firebaseapp.com",
-    projectId: "rayyan-h-tafe-nsw",
-    storageBucket: "rayyan-h-tafe-nsw.appspot.com",
-    messagingSenderId: "579661195251",
-    appId: "1:579661195251:web:107f8e4fcd7879c1c45486"
+const firebaseConfig = {
+  apiKey: "AIzaSyBsRZZgJVa78hvpGBhgHtiIJ99BaxoDXaE",
+  authDomain: "rayyan-h-tafe-nsw-2.firebaseapp.com",
+  projectId: "rayyan-h-tafe-nsw-2",
+  storageBucket: "rayyan-h-tafe-nsw-2.appspot.com",
+  messagingSenderId: "63137248093",
+  appId: "1:63137248093:web:ef978bcd80e8993c31aeb4"
 }
-
